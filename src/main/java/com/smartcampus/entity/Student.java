ab609eb287df8f71dee6905f1118ae9edddde1aa
kalyanivelukar <kalyani.velukar22@gmail.com>
2026-09-19 17:51:46 +0530

    Add placement management backend

--- src/main/java/com/smartcampus/entity/Student.java
+++ src/main/java/com/smartcampus/entity/Student.java
@@ -8,6 +8,7 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.ManyToMany;
+import jakarta.validation.constraints.Email;
 
 @Entity
 public class Student {
