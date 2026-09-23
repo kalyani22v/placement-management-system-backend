9c1933c09e1f9f4fec286938aaf464a153be545e
Kalyani Velukar <kalyani.velukar22@gmail.com>
2026-09-23 22:08:49 +0530

    Update SmartCampusServiceImpl.java

--- src/main/java/com/smartcampus/serviceimpl/SmartCampusServiceImpl.java
+++ src/main/java/com/smartcampus/serviceimpl/SmartCampusServiceImpl.java
@@ -162,7 +162,6 @@ public class SmartCampusServiceImpl implements SmartCampusService {
 
 		return msg;
 	}
-	@Transactional(readOnly = true)
 	public String deleteCompany(String name) {
 
 		Company comp = companyrepo.findByName(name);
